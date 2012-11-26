07a41b3252237a4993bd193fea89b53c248fbd3a
Sugeng Hariyanto <sugeng1225@gmail.com>
2012-11-26 11:33:08 +0700

    beta

--- SiRaJa/src/com/siraja/SirajaActivity.java
+++ SiRaJa/src/com/siraja/SirajaActivity.java
@@ -1,7 +1,6 @@
 package com.siraja;
 
 import android.os.Bundle;
-import android.view.Menu;
 import org.apache.cordova.*;
 
 public class SirajaActivity extends DroidGap {
@@ -11,12 +10,4 @@ public class SirajaActivity extends DroidGap {
         super.onCreate(savedInstanceState);
         super.loadUrl("file:///android_asset/www/index.html");
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_siraja, menu);
-        return true;
-    }
-    
 }
